a0b5b886e5e7484e996f32da5ef20551a2f5eccd
kalaiarasy <skalaiarasy@gmail.com>
2021-04-21 16:39:46 -0400

    from kalai

--- UsedCarLot/Program.cs
+++ UsedCarLot/Program.cs
@@ -8,6 +8,7 @@ namespace UsedCarLot
         {
             Console.WriteLine("Hi Welcome to our car Project!");
             Console.WriteLine("I've created the branch");
+            Console.WriteLine("I 've created the new branch kalai-car");
         }
     }
 }
