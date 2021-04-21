fd11aee7d7e91c7029e1fbfe7316cc878930d9cc
kalaiarasy <skalaiarasy@gmail.com>
2021-04-21 15:44:27 -0400

    I add a welcome message

--- UsedCarLot/Program.cs
+++ UsedCarLot/Program.cs
@@ -6,7 +6,7 @@ namespace UsedCarLot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi Welcome to our car Project!");
         }
     }
 }
